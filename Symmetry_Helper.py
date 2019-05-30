d59d7bc7d72e02a378037c18b79fa70226744c54
Tudor Tibu <tudortibu@gmail.com>
2019-05-30 00:23:10 -0400

    Added Symmetry Helper

---
+++ Symmetry_Helper.py
@@ -0,0 +1,25 @@
+'''
+Policy helper for the agents to play the game
+handles symmetry of the grids to reduce state soace size
+'''
+
+
+def findSymmetries(grid, grids=3, rowLength=3 ):
+    symmetrygrid = []
+
+    return
+
+
+def rotategrid90(grid, rowlength=3):
+    grids = []
+    tempgrid = grid
+    for itt in range (len(grid)):
+        print("thing")
+    return
+
+grid = []
+
+for itt in range (9):
+    grid.append( itt)
+
+rotategrid90(grid)
